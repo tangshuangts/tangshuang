63e5d9c2587fc2113de0b583a810905bcf8c2582
tangshuangts <15210532547@163.com>
2018-05-27 16:37:36 +0800

    测试“

--- scripts/test01.py
+++ scripts/test01.py
@@ -1,14 +1,15 @@
-import allure
-import pytest
+import pytest, allure
 
-class Test01:
+
+class Test_01:
     @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3])
-    def test_abc(self,a):
+    def test_abc(self, a):
 
         assert a != 2
 
     @allure.step(title="测试步骤002")
     @pytest.mark.parametrize("a", [1, 4, 3])
     def test_abc(self, a):
+        allure.attach('描述', '我是测试步骤002的描述')
         assert a != 2
